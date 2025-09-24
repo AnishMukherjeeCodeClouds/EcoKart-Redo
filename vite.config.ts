5340d4b6712f9b83f1360d482723e35693a55cf8
Anish Mukherjee <anish.mukherjee@codeclouds.co.in>
2025-09-24 11:52:58 +0530

    feat: add rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  server: {
-    allowedHosts: ["8e3839440254.ngrok-free.app"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        cart: "cart.html",
+        product: "product.html",
+        confirm: "confirm.html",
+      },
+    },
   },
 });
